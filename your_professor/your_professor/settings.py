c8409c219bbcd8a7c6c7ee23bcb08662fb29c774
leaf <krissthompson@protonmail.com>
2022-02-17 06:49:05 +0100

    remove graphene-django, add requirements file

--- your_professor/your_professor/settings.py
+++ your_professor/your_professor/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'your_professor_rest_api',
-    'graphene_django',
+    # 'graphene_django',
 ]
 
 MIDDLEWARE = [
@@ -93,9 +93,9 @@ config.DATABASE_URL = 'bolt://neo4j:3BejhhmCyUa4oPLm2XAgmX8GcsGqipFf9EtQvmPuo@lo
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
-GRAPHENE = {
-    "SCHEMA": "your_professor_rest_api.schema.schema"
-}
+# GRAPHENE = {
+#     "SCHEMA": "your_professor_rest_api.schema.schema"
+# }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
--- your_professor/your_professor_rest_api/urls.py
+++ your_professor/your_professor_rest_api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from graphene_django.views import GraphQLView
+# from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    # path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
+
 ]
