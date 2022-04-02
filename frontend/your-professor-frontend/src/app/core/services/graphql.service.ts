c45e5762681bf91b56fb9747e075c1aa2ebf9729
leaf <krissthompson@protonmail.com>
2022-04-02 19:37:57 +0200

    fix cors issues ( change localhost to http://127.0.0.1)

--- frontend/your-professor-frontend/src/app/core/services/graphql.service.ts
+++ frontend/your-professor-frontend/src/app/core/services/graphql.service.ts
@@ -2,23 +2,21 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {map, Observable} from "rxjs";
 
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class GraphqlService {
-
+  url = "http://127.0.0.1:7777/graphql/";
   constructor(private http: HttpClient) { }
-
-  public checkCors(){
-    return this.http.get("localhost:8000");
-  }
 
   public query<T>(options: {
     query: string;
     variables?: { [key: string]: any };
   }): Observable<T> {
     return this.http
-      .post<{ data: T }>(`localhost:7777/graphql`, {
+      .post<{ data: T }>(this.url, {
         query: options.query,
         variables: options.variables,
       })
@@ -30,7 +28,7 @@ export class GraphqlService {
     variables?: { [key: string]: any };
   }): Observable<any> {
     return this.http
-      .post<{ data: T }>(`/graphql`, {
+      .post<{ data: T }>(this.url, {
         query: options.mutation,
         variables: options.variables,
       })
--- frontend/your-professor-frontend/src/app/modules/countries/countries.component.ts
+++ frontend/your-professor-frontend/src/app/modules/countries/countries.component.ts
@@ -16,30 +16,27 @@ export class CountriesComponent implements OnInit {
   }
 
   query(){
-    console.log(this.graphqlService.checkCors().subscribe( res =>{
-      console.log(res);
-    }))
-    // let query = `
-    //       {
-    //         allCountries{
-    //           local_language_name
-    //         }
-    //       }
-    //   `;
-    // // console.log(query);
-    // // console.log(JSON.stringify(query));
-    // let subscription = this.graphqlService.query({
-    //   query: `
-    //       {
-    //         allCountries{
-    //           local_language_name
-    //         }
-    //       }
-    //   `
-    // }).subscribe( results => {
-    //   this.results = results;
-    // });
-    // console.log(this.results);
+    let query = `
+          {
+            allCountries{
+              local_language_name
+            }
+          }
+      `;
+    // console.log(query);
+    // console.log(JSON.stringify(query));
+    let subscription = this.graphqlService.query({
+      query: `
+          {
+            allCountries{
+              local_language_name
+            }
+          }
+      `
+    }).subscribe( results => {
+      this.results = results;
+    });
+    console.log(this.results);
   }
 
 }
